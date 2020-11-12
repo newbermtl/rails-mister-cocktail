1d59f1c7dc2a17e0dd8ffe8c89b0bdadff005d94
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-12 12:30:09 -0500

    filled ingredients from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+serialized = open(url).read
+
+ingredients = JSON.parse(serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!({ name: ingredient['strIngredient1'] })
+end
