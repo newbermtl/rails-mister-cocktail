c2b916c83441ff7e1bf334d68016d9812650e23d
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-12 16:25:29 -0500

    cocktails now showing with dose and ingredient list

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,16 +10,19 @@ class CocktailsController < ApplicationController
 
   def new
     @cocktail = Cocktail.new
-    @ingredient = Ingredient.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save!
 
+    redirect_to cocktails_path
   end
 
+  private
 
-  def destroy
-
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,27 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save!
+
+    redirect_to cocktail_path(@cocktail)
+
+  end
+
+  def delete
+    @dose = dose.find(params[:dose_id])
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
